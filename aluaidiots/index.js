3c044856f11280000a972cf119f1b442b7a7c7d5
Kanon <82295168+longlong233@users.noreply.github.com>
2023-04-16 07:12:14 +0800

    update

---
+++ aluaidiots/index.js
@@ -0,0 +1,16 @@
+function initImage() {
+    for (var ele of document.querySelectorAll(".image")) {
+        ele.onclick = function (event) {
+            var style = event.target.style;
+            if (style.cursor === "zoom-out") {
+                style.width = "60vw";
+                style.cursor = "zoom-in";
+            } else {
+                style.width = "100vw";
+                style.cursor = "zoom-out";
+            }
+        }
+    }
+}
+
+initImage();
