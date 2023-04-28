15b70206f24eef99dcbc07a7595b449a20b73be4
Kanon <82295168+longlong233@users.noreply.github.com>
2023-04-28 17:50:01 +0800

    update scrollbar

--- aluaidiots/index.js
+++ aluaidiots/index.js
@@ -1,5 +1,24 @@
 $ = mdui.$;
 
+Scrollbar.use(OverscrollPlugin);
+scrollbar = Scrollbar.init(document.querySelector(".scroll"), {
+    speed: 2.0,
+    damping: 0.2,
+    thumbMinSize: 24,
+    alwaysShowTracks: true,
+    overscrollEffect: 'bounce',
+    overscrollDamping: 0.4
+});
+
+function initAnchor() {
+    for (var ele of document.querySelectorAll("li>a")) {
+        ele.href = "javascript:void(0);";
+        ele.onclick = function (event) {
+            scrollbar.scrollTo(0, document.querySelector(event.target.dataset.scrollto).offsetTop, 500);
+        }
+    }
+}
+
 function initImage() {
     for (var ele of document.querySelectorAll(".image")) {
         ele.insertAdjacentHTML('afterend', '<p class="desc">' + ele.alt + '</p>');
@@ -16,4 +35,5 @@ function initImage() {
     }
 }
 
+initAnchor();
 initImage();
